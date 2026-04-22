e6a1597cdb8afba03e71c8c291ba9023d2f38b1f
Dev <fedorskarvinko@gmail.com>
2026-04-23 00:58:37 +0400

    add admin Habit

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from  .models import Habit
 
-# Register your models here.
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'habit_creator', 'habit', 'time', 'is_pleasant', 'periodicity',)
+    list_filter = ('is_pleasant', 'is_public', 'habit_creator',)
+    search_fields = ('habit', 'place', 'habit_creator__username',)
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('habit_creator', 'habit', 'place', 'time', 'duration')
+        }),
+        ('Логика привычки', {
+            'fields': ('is_pleasant', 'related_habit', 'reward', 'periodicity')
+        }),
+        ('Доступ', {
+            'fields': ('is_public',)
+        }),
+    )
